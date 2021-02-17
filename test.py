0c38100b6a0c16fcc6765f7537340247065aa45b
Helena Sousa <helenagarajau@hotmail.com>
2021-02-17 19:11:14 +0000

    Added Eval metrics to test

--- test.py
+++ test.py
@@ -7,9 +7,10 @@ import torchvision.datasets as dsets
 import torchvision.transforms as transforms
 import utils
 from arch import define_Gen, define_Dis
-
-
-
+import numpy as np
+from sklearn.metrics import mean_absolute_error
+from skimage.metrics import peak_signal_noise_ratio
+from calculate_fid import calculate_fid
 
 
 def test(args):
@@ -47,17 +48,41 @@ def test(args):
     a_real_test = Variable(iter(a_test_loader).next()[0], requires_grad=True)
     b_real_test = Variable(iter(b_test_loader).next()[0], requires_grad=True)
     a_real_test, b_real_test = utils.cuda([a_real_test, b_real_test])
-            
-
+    
+    # #calculate evaluation metrics
+    # dataiter_a = iter(a_test_loader)
+    # dataiter_b=iter(b_test_loader)
+    
+     
     Gab.eval()
     Gba.eval()
-
+    
+    list_T1_mae=[]
+    list_T1_psnr=[]
+    list_T1_fid=[]
+    
     with torch.no_grad():
-        a_fake_test = Gab(b_real_test)
+        a_fake_test = Gab(b_real_test) 
         b_fake_test = Gba(a_real_test)
         a_recon_test = Gab(b_fake_test)
         b_recon_test = Gba(a_fake_test)
 
+        list_T1_mae.append(mean_absolute_error(np.squeeze(b_real_test),np.squeeze(b_recon_test)))
+        list_T1_psnr.append(peak_signal_noise_ratio(np.squeeze(b_real_test),np.squeeze(b_recon_test)))
+        list_T1_fid.append(calculate_fid(np.squeeze(b_real_test),np.squeeze(b_recon_test)))
+        
+        
+        print("Mean of MAE = " + str(np.mean(list_T1_mae)))
+        print("Mean of PSNR = " + str(np.mean(list_T1_psnr)))
+        print("Mean of FID = " + str(np.mean(list_T1_fid)))
+
+        #print variance of MAE, PSNR, FID
+        print("Variance of MAE = " + str(np.var(list_T1_mae)))
+        print("Variance of PSNR = " + str(np.var(list_T1_psnr)))
+        print("Variance of FID = " + str(np.var(list_T1_fid)))
+        
+        
+# a_real_test= T2 real ; b_fake_test= T1 translated ; a_recon_test = T2 reconstructed | b_real_test= T1 real ; a_fake_test = T2 translated ; b_recon_test= T1 reconstructed
     pic = (torch.cat([a_real_test, b_fake_test, a_recon_test, b_real_test, a_fake_test, b_recon_test], dim=0).data + 1) / 2.0
 
     if not os.path.isdir(args.results_dir):
